9c6f4c80112fa9ced9739273c2f420fceae031d3
SHORYA BAJ <jain.shorya11@gmail.com>
2024-07-28 01:42:58 +0530

    zod

--- common/src/index.ts
+++ common/src/index.ts
@@ -5,14 +5,12 @@ export const signupInput = z.object({
   password: z.string().min(6),
   name: z.string().optional(),
 });
-
 export type SignupInput = z.infer<typeof signupInput>;
 
 export const signinInput = z.object({
   username: z.string().email(),
   password: z.string().min(6),
 });
-
 export type SigninInput = z.infer<typeof signinInput>;
 
 export const createBlogInput = z.object({
